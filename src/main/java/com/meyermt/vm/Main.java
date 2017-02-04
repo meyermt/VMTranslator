14114dc4e09795c52f21d522e41c2acfa4a2defd
Michael Meyer <meyermt@uchicago.edu>
2017-02-03 20:34:42 -0600

    stub out the beginning of what will be the parser

--- src/main/java/com/meyermt/vm/Main.java
+++ src/main/java/com/meyermt/vm/Main.java
@@ -1,6 +1,7 @@
 package com.meyermt.vm;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by michaelmeyer on 2/3/17.
@@ -8,8 +9,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        // read the .vm file in
         VMFileReader reader = new VMFileReader(args[0]);
         List<String> cleanFileLines = reader.readAndClean();
+
+        // stream over the file, parse and translate
+        AsmCoder coder = new AsmCoder();
+        Parser parser = new Parser(coder);
+        List<String> assemblerOutput = cleanFileLines.stream()
+                .map(line -> parser.parseAndTranslate(line))
+                .collect(Collectors.toList());
+
+        // write the output
     }
 
 }
--- src/main/java/com/meyermt/vm/Parser.java
+++ src/main/java/com/meyermt/vm/Parser.java
@@ -4,4 +4,14 @@ package com.meyermt.vm;
  * Created by michaelmeyer on 2/3/17.
  */
 public class Parser {
+
+    private final AsmCoder coder;
+
+    public Parser(AsmCoder coder) {
+        this.coder = coder;
+    }
+
+    public String parseAndTranslate(String vmLine) {
+
+    }
 }
